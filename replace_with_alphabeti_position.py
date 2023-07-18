4fdf91800eb9c5d4305b7077f582dbdb2ed21648
Michael White <hrt666.cs@gmail.com>
2023-07-18 10:31:56 +0900

    add description  for replace with alphabet position

--- replace_with_alphabeti_position.py
+++ replace_with_alphabeti_position.py
@@ -25,3 +25,11 @@ def alphabet_position(text):
 #                     n = 0
 #                     break
 #     return ' '.join(l)
+
+
+# DESCRIPTION:
+# Welcome.
+# In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+# If anything in the text isn't a letter, ignore it and don't return it.
+# "a" = 1, "b" = 2, etc.
+# https://www.codewars.com/kata/546f922b54af40e1e90001da
